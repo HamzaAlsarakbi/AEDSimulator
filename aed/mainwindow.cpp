a93ffc04fa5589ca1b9e15fc363f7e5efe39352e
Abdullah Aswad <abdullahaswad2003@gmail.com>
2023-12-07 17:08:21 -0500

    Some documentation on MainWindow + Updated Use Cases

--- aed/mainwindow.cpp
+++ aed/mainwindow.cpp
@@ -3,6 +3,10 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+/**
+ * @brief Construct a new Main Window:: Main Window object
+ * 
+ */
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -15,15 +19,31 @@ MainWindow::MainWindow(QWidget *parent)
     connectUI();
     update(aedDevice->getStatus());
     ui->depthSlider->setDisabled(true);
-//    emit drawEcg({1000, 2000, 3000, 4000, 5000, 6000});
+    emit drawEcg({
+        Pulse(milliseconds(1000), PULSE_NORMAL),
+        Pulse(milliseconds(2000), PULSE_NORMAL),
+        Pulse(milliseconds(3000), PULSE_NORMAL),
+        Pulse(milliseconds(4000), PULSE_NORMAL),
+        Pulse(milliseconds(5000), PULSE_NORMAL),
+        Pulse(milliseconds(6000), PULSE_NORMAL),
+    });
 }
 
+/**
+ * @brief Destroy the Main Window:: Main Window object
+ * 
+ */
 MainWindow::~MainWindow()
 {
     delete ecgWidget;
     delete ui;
     delete aedDevice;
 }
+
+/**
+ * @brief Connects teh buttons on the ui to signals that emit to do the actual functions
+ * 
+ */
 void MainWindow::connectUI() {
     // Sliders
     connect(ui->batterySlider, &QSlider::valueChanged, this, &MainWindow::batterySliderHandler);
@@ -31,8 +51,7 @@ void MainWindow::connectUI() {
     connect(ui->baseDepthVarianceSlider, &QSlider::valueChanged, this, &MainWindow::baseDepthVarianceSliderHandler);
     connect(ui->ageSlider, &QSlider::valueChanged, this, &MainWindow::ageSliderHandler);
     connect(ui->startingConditionBox, SIGNAL(currentTextChanged(QString)), this, SLOT(patientStartingConditionHandler(QString)));
-//    connect(this, SIGNAL(drawEcg(std::vector<long long>)), ecgWidget, SLOT(draw(std::vector<long long>)));
-    connect(this, SIGNAL(drawEcg(int)), ecgWidget, SLOT(draw(int)));
+    connect(this, SIGNAL(drawEcg(std::vector<Pulse>)), ecgWidget, SLOT(draw(std::vector<Pulse>)));
 
     // Buttons
     connect(ui->turnOnButton, &QPushButton::pressed, this, &MainWindow::turnOnHandler);
@@ -42,11 +61,16 @@ void MainWindow::connectUI() {
     connect(ui->failTestButton, &QPushButton::pressed, this, &MainWindow::failTestHandler);
     connect(ui->administerShockButton, &QPushButton::pressed, this, &MainWindow::administerShockHandler);
     connect(ui->cprButton, &QPushButton::pressed, this, &MainWindow::cprHandler);
-    connect(ui->cprButton, &QPushButton::pressed, this, &MainWindow::cprHandler);
     connect(aedDevice, SIGNAL(update(AEDStatus)), this, SLOT(update(AEDStatus)));
     connect(aedDevice, SIGNAL(updateDisplay(std::string)), this, SLOT(updateDisplay(std::string)));
     connect(aedDevice, SIGNAL(initUpdateHeartRate()), this, SLOT(updateHeartRate()));
 }
+
+/**
+ * @brief Updates the UI to fit the current state of the AED 
+ * 
+ * @param status (AEDStatus) The current state the AED is in
+ */
 void MainWindow::update(AEDStatus status) {
     const QString RED = QString::fromStdString("background-color: rgb(255, 100, 100)");
     const QString GREEN = QString::fromStdString("background-color: rgb(100, 255, 100)");
@@ -63,7 +87,7 @@ void MainWindow::update(AEDStatus status) {
     ui->heartRateLabel->setVisible(status > 8);
     ui->batteryLabel->setText(status < 2 ? "" : QString::fromStdString(std::to_string(aedDevice->getBattery()) + "%"));
     ui->shocksCount->setText(status < 2 ? "" : QString::number(aedDevice->getShocksCount()));
-    ecgWidget->setVisible(status > 8);
+    // ecgWidget->setVisible(status > 8);
 //    AED Lights
     ui->testPassIndicator->setStyleSheet(status == AED_OFF || status == AED_ON ? GRAY : aedDevice->isPassing() ? GREEN : RED);
     for(int i = 0; i < indicators.size(); i++) {
@@ -92,32 +116,64 @@ void MainWindow::update(AEDStatus status) {
     ui->heartStatusWidget->setVisible(status > 8);
 }
 
+/**
+ * @brief displays the text representing the state of the AED
+ * 
+ * @param text (string) The text to be displayed
+ */
 void MainWindow::updateDisplay(std::string text) {
     ui->displayLabel->setText(QString::fromStdString(text));
 }
+
+/**
+ * @brief Updates the UI depending on the state of the heart
+ * 
+ */
 void MainWindow::updateHeartRate() {
     if(aedDevice->isHeartNull()) return;
     ui->heartRateLabel->setText(aedDevice->getHeartRate() == -1 ? "" : QString::number(aedDevice->getHeartRate()));
     ui->qrsWidthLabel->setText(QString::number(aedDevice->getBasePulseTime()));
     ui->qrsWidthVarianceLabel->setText(QString::number(aedDevice->getPulseTimeVariance()));
     ui->pulsesCountLabel->setText(QString::number(aedDevice->getPulsesCount()));
-    std::vector<std::string> statuses = { "NORMAL", "VTACH", "VFIB", "ARISTOTLE" };
+    std::vector<std::string> statuses = { "NORMAL", "VTACH", "VFIB", "ASYSTOLE" };
     ui->heartStatusLabel->setText(QString::fromStdString(statuses.at(aedDevice->getHeartStatus())));
-    emit drawEcg(aedDevice->getHeartRate());
+    // emit drawEcg(aedDevice->getHeartRate());
 }
 
+/**
+ * @brief updates the value of battery 
+ * 
+ * @param value (int) The value of the battery from the slider
+ */
 void MainWindow::batterySliderHandler(int value) {
     aedDevice->setBattery(value);
     ui->batterySliderLabel->setText(QString::number(value, 'd', 0));
     ui->batteryLabel->setText(QString::fromStdString(std::to_string(value) + "%"));
 }
 
+/**
+ * @brief Updates the value of compression depth
+ * 
+ * @param value (int) The value of the compression depth from the slider
+ */
 void MainWindow::baseDepthSliderHandler(int value) {
     ui->baseDepthLabel->setText(QString::number(value / 100.0, 'f', 2));
 }
+
+/**
+ * @brief Updates the value of the compression depth variance
+ * 
+ * @param value (int) the radius of the compression depth from the slider
+ */
 void MainWindow::baseDepthVarianceSliderHandler(int value) {
     ui->baseDepthVarianceLabel->setText(QString::number(value / 100.0, 'f', 2));
 }
+
+/**
+ * @brief Updates the value of the compression depth variance
+ * 
+ * @param value (int) The value of the patient's age from the slider
+ */
 void MainWindow::ageSliderHandler(int value) {
     ui->ageLabel->setText(QString::number(value, 'd', 0));
     aedDevice->setAge(value);
@@ -127,7 +183,7 @@ void MainWindow::patientStartingConditionHandler(QString condition) {
     std::string c = condition.toStdString();
     aedDevice->resetPatient(
         c == "Normal" ? PSC_NORMAL :
-        c == "Aristotle" ? PSC_ARISTOTLE :
+        c == "Asystole" ? PSC_ASYSTOLE :
         c == "Sub 40" ? PSC_SUB40 :
         PSC_HEART_ATTACK);
 }
